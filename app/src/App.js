f82f276d69ed9ec8046ee2d385526e2f5737da16
Jake Hartnell <jakehartnell@hypothes.is>
2018-02-10 15:11:24 -0800

    basic photo upload.

--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ImageUploader from 'react-images-upload';
+import { Spin } from 'antd';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { pictures: [] };
+    this.onDrop = this.onDrop.bind(this);
+  }
+
+  onDrop(picture) {
+    console.log(picture);
+    this.setState({
+      pictures: this.state.pictures.concat(picture),
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Canapest</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Welcome to the Canabis plant disease classifier, upload an image to classify it.
         </p>
+        <ImageUploader
+            withIcon={true}
+            buttonText='Upload image'
+            onChange={this.onDrop}
+            imgExtension={['.jpg', '.gif', '.png', '.gif']}
+            maxFileSize={5242880}
+            withPreview={true}
+            withLabel={false}
+        />
+        <Spin size="large" />
       </div>
     );
   }
